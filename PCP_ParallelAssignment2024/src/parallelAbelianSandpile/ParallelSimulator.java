45bf249c38a156f22145ee37df15a7ece452be87
Nancia Mwaramba <mwrnan002@>
2024-08-11 15:04:29 +0200

    made updates to the Parallel Simulator class so that it implements the paralel updates

---
+++ PCP_ParallelAssignment2024/src/parallelAbelianSandpile/ParallelSimulator.java
@@ -0,0 +1,108 @@
+package parallelAbelianSandpile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.ForkJoinPool;
+/* Serial  program to simulate an Abelian Sandpile cellular automaton
+ * This is the reference sequential version (Do not modify this code)
+ * Michelle Kuttel 2024, University of Cape Town
+ * Adapted from "Abelian Sandpile Simulation"
+ * Peachy Parallel Assignments (EduPar 2022)" 
+ * developed by Bu\:cker, Casanova and Da Silva  (∗Institute for Computer Science, Friedrich Schiller University Jena, Jena, Germany)
+ */
+ 
+ import parallelAbelianSandpile.Grid;
+
+ public class ParallelSimulator {
+    static final boolean DEBUG = false; // for debugging output, off
+
+    static long startTime = 0;
+    static long endTime = 0;
+
+    // timers - note milliseconds
+    private static void tick() { // start timing
+        startTime = System.currentTimeMillis();
+    }
+
+    private static void tock() { // end timing
+        endTime = System.currentTimeMillis();
+    }
+	//input is via a CSV file
+	 public static int[][] readArrayFromCSV(String filePath) {
+        int[][] array = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = br.readLine();
+            if (line != null) {
+                String[] dimensions = line.split(",");
+                int width = Integer.parseInt(dimensions[0]);
+                int height = Integer.parseInt(dimensions[1]);
+                System.out.printf("Rows: %d, Columns: %d\n", width, height); // Do NOT CHANGE - you must output this
+
+                array = new int[height][width];
+                int rowIndex = 0;
+
+                while ((line = br.readLine()) != null && rowIndex < height) {
+                    String[] values = line.split(",");
+                    for (int colIndex = 0; colIndex < width; colIndex++) {
+                        array[rowIndex][colIndex] = Integer.parseInt(values[colIndex]);
+                    }
+                    rowIndex++;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return array;
+	}
+
+	public static boolean updateGrid(Grid grid, ForkJoinPool pool) {
+        ParallelUpdate task = new ParallelUpdate(grid, 1, grid.getRows() + 1);
+        boolean change = pool.invoke(task);
+        if (change) {
+            grid.nextTimeStep();
+        }
+        return change;
+    }
+	 
+    public static void main(String[] args) throws IOException {
+
+        Grid simulationGrid; // the cellular automaton grid
+
+        if (args.length != 2) { // input is the name of the input and output files
+            System.out.println("Incorrect number of command line arguments provided.");
+            System.exit(0);
+        }
+        /* Read argument values */
+        String inputFileName = args[0]; // input file name
+        String outputFileName = args[1]; // output file name
+
+        // Read from input .csv file
+        simulationGrid = new Grid(readArrayFromCSV(inputFileName));
+
+        int counter = 0;
+        tick(); // start timer
+        if (DEBUG) {
+            System.out.printf("starting config: %d \n", counter);
+            simulationGrid.printGrid();
+        }
+
+        ForkJoinPool pool = new ForkJoinPool();
+
+        while (updateGrid(simulationGrid, pool)) { // run until no change
+            if (DEBUG)
+                simulationGrid.printGrid();
+            counter++;
+        }
+        tock(); // end timer
+
+        System.out.println("Simulation complete, writing image...");
+        simulationGrid.gridToImage(outputFileName); // write grid as an image - you must do this.
+        // Do NOT CHANGE below!
+        // simulation details - you must keep these lines at the end of the output in the parallel versions
+        System.out.printf("\t Rows: %d, Columns: %d\n", simulationGrid.getRows(), simulationGrid.getColumns());
+        System.out.printf("Number of steps to stable state: %d \n", counter);
+        System.out.printf("Time: %d ms\n", endTime - startTime); /* Total computation time */
+    }
+}
